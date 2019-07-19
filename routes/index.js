f73ecf0d5b788459707091271ff734b68b1650bd
kev <kelvinchege@gmail.com>
2019-07-19 13:39:49 +0300

    data is formatted

--- routes/index.js
+++ routes/index.js
@@ -15,25 +15,25 @@ router.get('/', function(req, res, next) {
 //  FETCH CATEGORIES FOR BUSINESSES
 router.get('/general/categories', async (req, res, next)=> {
   let categories = await Category.find({approved: true,group: 'general'}).sort([['order', 1]]);
-  res.status(200).json(categories);
+  res.status(200).json({code: 100,data: categories});
 })
 
 //  FETCH CATEGORIES FOR PRODUCTS PART
 router.get('/shopping/categories', async (req, res, next)=> {
   let categories = await Category.find({group: 'shopping'}).sort([['order', 1]]);
-  res.status(200).json(categories);
+  res.status(200).json({code: 100, data: categories});
 })
 
 router.get('/coupons', async(req, res, next)=> {
   let coupons = await Coupons.find({
     status: true
   }).populate('bizid').sort([['order', 1], ['star', -1]]).limit(10);
-  res.status(200).json(coupons);
+  res.status(200).json({code: 100, data: coupons});
 })
 
 router.get('/reviews', async(req, res, next)=> {
   let reviews = await Reviews.find().sort([['created_at', -1]]).populate('bizid').populate('user_id').limit(5);
-  res.status(200).json(reviews);
+  res.status(200).json({code: 100,data: reviews});
 })
 
 module.exports = router;
